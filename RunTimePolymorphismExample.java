b27475f9bd1b6d6f8dc68935f718b0bb8e760dfe
Suneel Kumar G <64411726+gudipatisuneel@users.noreply.github.com>
2024-02-21 20:03:45 +0530

    Add files via upload

---
+++ RunTimePolymorphismExample.java
@@ -0,0 +1,9 @@
+public class RunTimePolymorphismExample {
+    public static void main(String[] args) {
+        Employee prog = new Programmer(1, "Rohit", "Programmer - Coding", 1);
+        System.out.println(prog.toString());
+        
+        Employee mgr = new Manager(2, "Rahul", "Manager - Management", 4);
+        System.out.println(mgr.toString());
+    }
+}
